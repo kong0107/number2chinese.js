212ac9021b606f6aad5e2794229014dd353ed7c1
kong0107 <kong0107@gmail.com>
2018-03-06 18:51:42 +0800

    supports negative integers

---
+++ README.md
@@ -0,0 +1,18 @@
+# convert number to Chinese strings
+
+## usage
+
+```html
+<script src="number2chinese.js"></script>
+<script>
+  number = -326;
+  console.log(number.toChinese()); //<- 負三百二十六
+  console.log(number.toChinese("simplified", "upper")); //<- 负叁佰贰拾陆
+</script>
+```
+
+[try by yourself](https://kong0107.github.io/number2chinese.js/)
+
+## note
+
+only supports integers
--- number2chinese.js
+++ number2chinese.js
@@ -2,11 +2,13 @@ Number.prototype.toChinese = function() {
     const characters = {
         traditional: {
             lower: {
+                minus: "負",
                 digits: "零一二三四五六七八九",
                 tens: "十百千",
                 wans: "萬億兆京垓秭穰溝澗正載"
             },
             upper: {
+                minus: "負",
                 digits: "零壹貳參肆伍陸柒捌玖",
                 tens: "拾佰仟",
                 wans: "萬億兆京垓秭穰溝澗正載"
@@ -14,11 +16,13 @@ Number.prototype.toChinese = function() {
         },
         simplified: {
             lower: {
+                minus: "负",
                 digits: "零一二三四五六七八九",
                 tens: "十百千",
                 wans: "万亿兆京垓秭穰沟涧正载"
             },
             upper: {
+                minus: "负",
                 digits: "零壹贰叁肆伍陆柒捌玖",
                 tens: "拾佰仟",
                 wans: "万亿兆京垓秭穰沟涧正载"
@@ -31,27 +35,27 @@ Number.prototype.toChinese = function() {
     return function(script, set) {
         if(!script || !characters[script]) script = "traditional";
         if(!set || !characters[script][set]) set = "lower";
-        const digits = characters[script][set].digits;
-        const tens = characters[script][set].tens;
-        const wans = characters[script][set].wans;
+        const c = characters[script][set];
         
-        let result = "";
-        let arr = this.toString().split("").map(d=>parseInt(d)).reverse();
-        for(let i = 0; i < arr.length; ++i) {
-            let d = arr[i];
-            let t = i % 4;
-            let w = Math.floor(i / 4);
-            
-            if(t) {
-                if(d) result += tens.charAt(t-1);
+        let value = this.valueOf();
+        let result = "", temp = "";
+        let arr = Math.abs(value).toString().split("").map(d=>parseInt(d)).reverse();
+        for(let w = 0; w < arr.length / 4; ++w) {
+            for(let t = 0; t < 4; ++t) {
+                let i = w * 4 + t;
+                if(i == arr.length) break;
+                let d = arr[i];
+                
+                if(t && d) temp += c.tens.charAt(t-1);
+                if(d || (t && arr[i-1])) temp += c.digits.charAt(d);
             }
-            else if(w) result += wans.charAt(w-1);
-            
-            if(d) result += digits.charAt(d);
-            else {
-                if(t && i && arr[i-1]) result += digits.charAt(0);
+            if(temp) {
+                if(w) result += c.wans.charAt(w-1);
+                result += temp; 
+                temp = "";
             }
         }
+        if(value < 0) result += c.minus;
         return result.split("").reverse().join("");
     }
 }();
